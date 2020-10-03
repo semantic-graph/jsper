6e2804f68f039ccce538e6d2f3805dcc6ecc0589
Zhen Zhang <git@zhen-zhang.com>
2020-10-02 21:22:32 -0700

    Remove graph code

--- main.ts
+++ main.ts
@@ -4,27 +4,9 @@ import * as esprima from "esprima";
 let inputPath = process.argv[2];
 let source = fs.readFileSync(inputPath, 'utf8')
 
-var edges = [];
-var nodes = [];
-
-function getNode(name: String) {
-    let idx = nodes.indexOf(name);
-    if (idx == -1) {
-        idx = nodes.length;
-        nodes.push(name);
-        return idx;
-    }
-    return idx;
-}
-
-function freshNode(name: String) {
-    let idx = nodes.length;
-    nodes.push(name);
-    return idx;
-}
-
 esprima.parseScript(source, {}, (node, _) => {
     if (node.type === 'CallExpression') {
+        console.log(node);
         switch (node.callee.type) {
             case "MemberExpression":
                 if (node.callee.property.type == "Identifier") {
@@ -32,17 +14,8 @@ esprima.parseScript(source, {}, (node, _) => {
                     if (node.callee.object.type == "Identifier" && node.callee.object.name == "Buffer") { // HACK: we will use WALA to generalize this in future
                         name = node.callee.object.name + "." + name;
                     }
-                    edges.push([getNode("__main__"), freshNode(name)]);
                 }
                 break;
         }
     }
 });
-
-fs.writeFileSync(inputPath + ".js2graph.json", JSON.stringify({
-    nodes: nodes,
-    edges: edges,
-    meta: {
-        "input": inputPath
-    }
-}));
