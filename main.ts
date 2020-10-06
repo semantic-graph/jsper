8c95e186aa8aefd3240bcfd79c0ab52a0a1d2044
Zhen Zhang <7168454+izgzhen@users.noreply.github.com>
2020-10-06 16:33:23 -0700

    Support more type of PE (#2)

--- main.ts
+++ main.ts
@@ -1,25 +1,16 @@
 import * as fs from "fs";
 import * as esprima from "esprima";
 import * as ESTree from "estree";
-import { start } from "repl";
 import { assert } from "console";
-import { NONAME } from "dns";
-
-let inputPath = process.argv[2];
-let source = fs.readFileSync(inputPath, 'utf8')
 
 const {NodeVM} = require('vm2');
 const vm = new NodeVM({
     wrapper: "none"
 });
 
-let sourceLines = source.split("\n")
+let loadedDefs = new Map();
 
-let loadedFuncs = new Map();
-
-let rewrites: { from: string, to: string }[] = [];
-
-function getFragment(node: ESTree.Node) {
+function getFragment(sourceLines: string[], node: ESTree.Node) {
     if (node.loc) {
         let startLoc = node.loc.start;
         let endLoc = node.loc.end;
@@ -38,44 +29,125 @@ function getFragment(node: ESTree.Node) {
     throw new Error("node has no loc.")
 }
 
-esprima.parseScript(source, {loc: true}, (node, _) => {
-    if (node.type === 'FunctionDeclaration' && node.id) {
-        let fragment = getFragment(node);
-        console.log("---- Loading " + node.id.name + " -----\n" + fragment);
-        vm.run(fragment);
-        assert(!loadedFuncs.has(node.id.name));
-        loadedFuncs.set(node.id.name, fragment);
+function rewriteOnce(source: string) {
+    let sourceLines = source.split("\n");
+
+    let rewrites: { from: string, to: string }[] = [];
+
+    function evalFragment(fragmentToRun: string) {
+        try {
+            return vm.run(fragmentToRun);
+        } catch (error) {
+            console.log("VM eval error:" + error);
+            return undefined;
+        }
     }
-    if (node.type === 'CallExpression') {
-        switch (node.callee.type) {
-            case "Identifier":
-                if (loadedFuncs.has(node.callee.name)) {
-                    let fragment = getFragment(node);
-                    let allFragments = Array.from(loadedFuncs.values()).join("\n") + "\nreturn " + fragment;
-                    console.log("---- Running ----\n" + allFragments);
-                    let result = vm.run(allFragments);
-                    console.log("---- Result ----\n", result);
+
+    function evalRewriteNode(node: ESTree.Node, withDefs: boolean = true) {
+        let originalFragment = getFragment(sourceLines, node);
+        var fragmentToRun;
+        if (withDefs) {
+            fragmentToRun = Array.from(loadedDefs.values()).join("\n") + "\nreturn " + originalFragment;
+        } else {
+            fragmentToRun = "return " + originalFragment;
+            // console.log("---- Running Fragment ----\n" + fragmentToRun);
+        }
+       let result = evalFragment(fragmentToRun);
+        if (result != undefined) {
+            // console.log("---- Result ----\n", result);
+            rewrites.push({
+                from: originalFragment,
+                to: JSON.stringify(result)
+            })
+        }
+    }
+
+    esprima.parseScript(source, {loc: true}, (node, _) => {
+        if (node.type === 'CallExpression') {
+            switch (node.callee.type) {
+                case "Identifier":
+                    if (loadedDefs.has(node.callee.name)) {
+                        evalRewriteNode(node);
+                    }
+                    break;
+            }
+        }
+        if (node.type === 'MemberExpression') {
+            switch (node.object.type) {
+                case "Identifier":
+                    if (loadedDefs.has(node.object.name)) {
+                        evalRewriteNode(node);
+                    }
+                    break;
+            }
+        }
+        if (node.type === "BinaryExpression") {
+            if (node.left.type === "Literal" &&
+                node.right.type === "Literal") {
+                evalRewriteNode(node, false);
+            }
+            if (node.left.type === "BinaryExpression" &&
+                node.left.right.type === "Literal" &&
+                node.right.type === "Literal" &&
+                node.left.operator === "+" &&
+                node.operator === "+") {
+                let nodeFragment = getFragment(sourceLines, node);
+                let nodeLLFragment = getFragment(sourceLines, node.left.left);
+                let nodeLRFragment = getFragment(sourceLines, node.left.right);
+                let nodeRFragment = getFragment(sourceLines, node.right);
+                var fragmentToRun ="return " + nodeLRFragment + "+" + nodeRFragment;
+                // console.log("---- Running Fragment ----\n" + fragmentToRun);
+                let result = evalFragment(fragmentToRun);
+                if (result != undefined) {
+                    // console.log("---- Result ----\n", result);
                     rewrites.push({
-                        from: fragment,
-                        to: JSON.stringify(result)
+                        from: nodeFragment,
+                        to: nodeLLFragment + "+" + JSON.stringify(result)
                     })
                 }
-                break;
-            case "MemberExpression":
-                if (node.callee.property.type == "Identifier") {
-                    var name = node.callee.property.name;
-                    if (node.callee.object.type == "Identifier" && node.callee.object.name == "Buffer") { // HACK: we will use WALA to generalize this in future
-                        name = node.callee.object.name + "." + name;
-                    }
-                }
-                break;
+            }
         }
+    });
+    var outputSource = source;
+    for (let rewrite of rewrites) {
+        outputSource = outputSource.replace(rewrite.from, rewrite.to);
+    }
+    return {
+        rewrites: rewrites,
+        outputSource: outputSource
+    };
+}
+
+let inputPath = process.argv[2];
+var source = fs.readFileSync(inputPath, 'utf8')
+
+let sourceLines = source.split("\n");
+esprima.parseScript(source, {loc: true}, (node, _) => {
+    if (node.type === 'AssignmentExpression') {
+        let fragment = getFragment(sourceLines, node);
+        if (node.right.type === 'Literal' && node.left.type === "Identifier") {
+            // console.log("---- Loading assignment to literal " + node.left.name + " -----\n" + fragment);
+            assert(!loadedDefs.has(node.left.name));
+            loadedDefs.set(node.left.name, fragment);
+        }
+    }
+    if (node.type === 'FunctionDeclaration' && node.id) {
+        let fragment = getFragment(sourceLines, node);
+        // console.log("---- Loading " + node.id.name + " -----\n" + fragment);
+        assert(!loadedDefs.has(node.id.name));
+        loadedDefs.set(node.id.name, fragment);
     }
 });
 
-var outputSource = source;
-for (let rewrite of rewrites) {
-    outputSource = source.replace(rewrite.from, rewrite.to);
+let totalSteps = 1000000;
+var step = 0;
+for (; step < totalSteps; step++) {
+    let result = rewriteOnce(source);
+    if (result.rewrites.length === 0) break;
+    console.log("--- Rewrite ---")
+    source = result.outputSource;
 }
-console.log("--- Rewritten source ---")
-console.log(outputSource);
+
+console.log(source);
+
+console.log("Run steps:", step);
